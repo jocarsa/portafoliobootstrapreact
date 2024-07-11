af5da745b50d420f8d37eef13b6d5f33cafd0fec
jocarsa <info@josevicentecarratala.com>
2024-07-11 11:42:39 +0200

    3

--- portafolio/src/Partes/Fichas/Ficha/Ficha.js
+++ portafolio/src/Partes/Fichas/Ficha/Ficha.js
@@ -1,18 +1,19 @@
 import './ficha.css';
 
-function Ficha() {
+function Ficha({titulo,contenido,tiempo}) {
+    
   return (
       <div class="col">
               <div class="card shadow-sm">
-                <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text></svg>
+                <svg class="bd-placeholder-img card-img-top" width="100%" height="225" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder: Thumbnail" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="#55595c"/><text x="50%" y="50%" fill="#eceeef" dy=".3em">{titulo}</text></svg>
                 <div class="card-body">
-                  <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p class="card-text">{contenido}</p>
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
                       <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
                       <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
                     </div>
-                    <small class="text-body-secondary">9 mins</small>
+                    <small class="text-body-secondary">{tiempo} mins</small>
                   </div>
                 </div>
               </div>
--- portafolio/src/Partes/Fichas/Fichas.js
+++ portafolio/src/Partes/Fichas/Fichas.js
@@ -6,17 +6,18 @@ function Fichas() {
         <div class="container">
 
           <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
-                <Ficha />
-              <Ficha />
-              <Ficha />
-              <Ficha />
-              <Ficha />
-              <Ficha />
-              <Ficha />
-              <Ficha />
-              <Ficha />
-              <Ficha />
-              <Ficha />
+                <Ficha titulo="ficha1" contenido="hola" tiempo="1"/>
+              <Ficha titulo="ficha2" contenido="hola"  tiempo="2"/>
+              <Ficha titulo="ficha3" contenido="hola"  tiempo="3"/>
+              <Ficha titulo="ficha4" contenido="hola"  tiempo="4"/>
+              <Ficha titulo="ficha5" contenido="hola"  tiempo="5"/>
+              <Ficha titulo="ficha6" contenido="hola"  tiempo="6"/>
+              <Ficha titulo="ficha7" contenido="hola"  tiempo="7"/>
+              <Ficha titulo="ficha8" contenido="hola"  tiempo="8"/>
+              <Ficha titulo="ficha9" contenido="hola"  tiempo="9"/>
+              <Ficha titulo="ficha10" contenido="hola"  tiempo="10"/>
+              <Ficha titulo="ficha11" contenido="hola"  tiempo="11"/>
+                <Ficha titulo="ficha12" contenido="hola"  tiempo="12"/>
           </div>
         </div>
       </div>
